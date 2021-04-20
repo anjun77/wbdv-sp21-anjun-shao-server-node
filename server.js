c0d935fec58140c9d7907e7587974c688078c954
anjun77 <anjunshao93@gmail.com>
2021-04-20 12:33:10 -0700

    server

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ app.use(bodyParser.json())
 require('./controllers/demo-controller')(app)
 require('./controllers/quizzes-controller')(app)
 require('./controllers/question-controller')(app)
+require('./controllers/quiz-attempts-controller')(app)
 
 app.listen(process.env.PORT||4000)
 
